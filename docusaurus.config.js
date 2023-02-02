1b5e943def4c5ef93199b856d4e6b2353116e44d
Paul <paul.cartau@gmail.com>
2023-02-02 18:48:43 +0100

    update Edit this page
    Signed-off-by: Paul <paul.cartau@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Pcartau/42-randoms/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Pcartau/42-randoms/tree/master',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Pcartau/42-randoms/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Pcartau/42-randoms/tree/master',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
