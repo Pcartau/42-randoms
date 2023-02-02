23be08ed86e6f29166f85de39b59e0df822a131d
Paul <paul.cartau@gmail.com>
2023-02-02 17:42:34 +0100

    edit config
    Signed-off-by: Paul <paul.cartau@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: '42 Randoms',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -64,10 +64,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: '42 Randoms',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: '42 Randoms Logo',
+          src: 'img/logo.png',
         },
         items: [
           {
